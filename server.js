9860e898e31695b9185e18d5c67c5a298d39dc9f
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-07-29 00:25:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors({
 }));
 
 // Encryption settings
-const adminEmail = "joelrubby518@gmail.com";
+const adminEmail = "tukhelen1@gmail.com";
 const encryptionKey = "12345678901234567890123456789012";
 
 function encrypt(text) {
